8e71e40da768ae006b90af22a9dccfe3c6b3f741
AnHyeongryeol <reversiblehope@gmail.com>
2024-05-13 12:05:54 +0900

    Spring DATA JPA Querydsl Test

--- src/main/java/com/second/shop/repository/ItemRepository.java
+++ src/main/java/com/second/shop/repository/ItemRepository.java
@@ -4,9 +4,10 @@ import com.second.shop.entity.Item;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
 
   List<Item> findByItemNm(String itemNm);
 
