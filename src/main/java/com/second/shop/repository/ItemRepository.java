0e1ef61e5aa972dc1c2f340311659a769a579291
AnHyeongryeol <reversiblehope@gmail.com>
2024-05-13 11:47:12 +0900

    Spring DATA JPA @Query 어노테이션

--- src/main/java/com/second/shop/repository/ItemRepository.java
+++ src/main/java/com/second/shop/repository/ItemRepository.java
@@ -3,6 +3,8 @@ package com.second.shop.repository;
 import com.second.shop.entity.Item;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
@@ -13,4 +15,11 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
   List<Item> findByPriceLessThan(Integer price);
 
   List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+  @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+  List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+  @Query(value="select * from item i where i.item_detail like " +
+      "%:itemDetail% order by i.price desc", nativeQuery = true)
+  List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 }
