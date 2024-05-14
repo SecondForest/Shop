03b36433c3fd872c8b05278bcecebae40a2f1f39
AnHyeongryeol <reversiblehope@gmail.com>
2024-05-14 10:33:28 +0900

    SpringSecurity : Login 구현

--- src/main/java/com/second/shop/config/SecurityConfig.java
+++ src/main/java/com/second/shop/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.second.shop.config;
 
+import com.second.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,14 +9,27 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Autowired
+  MemberService memberService;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http.build();
+    http.formLogin()
+        .loginPage("/members/login")
+        .defaultSuccessUrl("/")
+        .usernameParameter("email")
+        .failureUrl("/members/login/error")
+        .and()
+        .logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/memebers/logout"))
+        .logoutSuccessUrl("/");
+  return http.build();
   }
 
   @Bean
